35deea7a5c5d922f2ee899016099bcc7ee1982ae
Jamie White <jamie@jgwhite.co.uk>
2013-09-29 16:25:30 +0100

    Instate FixtureAdapter as ApplicationAdapter

--- js/config/app.js
+++ js/config/app.js
@@ -1,11 +1,10 @@
-// require other, dependencies here, ie:
-// require('./vendor/moment');
-
 require('../vendor/jquery');
 require('../vendor/handlebars');
 require('../vendor/ember');
-require('../vendor/ember-data'); // delete if you don't want ember-data
+require('../vendor/ember-data');
 
 var App = Ember.Application.create();
+
+App.ApplicationAdapter = DS.FixtureAdapter;
 
 module.exports = App;
