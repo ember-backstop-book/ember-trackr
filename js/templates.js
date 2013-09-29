327949df47e08321ce4fd2686bf84121d42a8788
Jamie White <jamie@jgwhite.co.uk>
2013-09-29 20:20:13 +0100

    Add bootstrap grid to tickets template

--- js/templates.js
+++ js/templates.js
@@ -73,20 +73,24 @@ helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
 function program1(depth0,data) {
   
   var buffer = '', hashTypes, hashContexts;
-  data.buffer.push("\n    <li class=\"list-group-item\">");
+  data.buffer.push("\n        <li class=\"list-group-item\">");
   hashTypes = {};
   hashContexts = {};
   data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "title", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
-  data.buffer.push("</li>\n  ");
+  data.buffer.push("</li>\n      ");
   return buffer;
   }
 
-  data.buffer.push("<ul class=\"list-group\">\n  ");
+  data.buffer.push("<div class=\"row\">\n  <div class=\"col-md-4\">\n    <ul class=\"list-group\">\n      ");
   hashTypes = {};
   hashContexts = {};
   stack1 = helpers.each.call(depth0, {hash:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[],types:[],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-  data.buffer.push("\n</ul>\n");
+  data.buffer.push("\n    </ul>\n  </div>\n  <div class=\"col-md-8\">\n    ");
+  hashTypes = {};
+  hashContexts = {};
+  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "outlet", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  data.buffer.push("\n  </div>\n</div>\n");
   return buffer;
   
 });
