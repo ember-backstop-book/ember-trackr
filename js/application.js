e19662f28d5ab3709623546a7067e5714d9f6dd3
Jamie White <jamie@jgwhite.co.uk>
2013-10-02 08:28:08 +0100

    Add ticket route

--- js/application.js
+++ js/application.js
@@ -15,7 +15,7 @@ var App = require('./app');
 
 App.Router.map(function() {
   this.resource('tickets', function() {
-
+    this.resource('ticket', { path: ':ticket_id' });
   });
 });
 
--- js/config/routes.js
+++ js/config/routes.js
@@ -2,6 +2,6 @@ var App = require('./app');
 
 App.Router.map(function() {
   this.resource('tickets', function() {
-
+    this.resource('ticket', { path: ':ticket_id' });
   });
 });
