16234d9e299b1d65e232da745b1a02111a5867e7
Jamie White <jamie@jgwhite.co.uk>
2013-10-03 07:00:52 +0100

    Add failing ticket editing test

--- test/tickets_test.js
+++ test/tickets_test.js
@@ -22,3 +22,16 @@ test('viewing ticket details', function() {
        'expected to find ticket description');
   });
 });
+
+test('editing ticket details', function() {
+  visit('/tickets/1')
+  .click('button:contains("Edit")')
+  .fillIn('input[name="title"]', 'Foo Bar')
+  .click('button:contains("Done")')
+  .then(function() {
+    ok(find('.list-group-item:contains("Foo Bar")').length,
+       'expected title in master list to update');
+    ok(find('.panel-title:contains("Foo Bar")').length,
+       'expected title in detail view to update');
+  });
+});
