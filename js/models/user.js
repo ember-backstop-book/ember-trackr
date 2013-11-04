3c2a3cd20240d1fb97de96de7898a6ecdbac27a2
Jamie White <jamie@jgwhite.co.uk>
2013-11-04 14:37:21 +0100

    Add gravatarURL but no md5

--- js/models/user.js
+++ js/models/user.js
@@ -1,10 +1,18 @@
 var User = DS.Model.extend({
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
+  email: DS.attr('email'),
 
   displayName: function() {
     return this.get('firstName') + ' ' + this.get('lastName');
-  }.property('firstName', 'lastName')
+  }.property('firstName', 'lastName'),
+
+  gravatarURL: function() {
+    var email = this.get('email'),
+        computedMD5 = md5(email);
+
+    return 'http://www.gravatar.com/avatar/%@.jpg?s=80'.fmt(computedMD5);
+  }.property('email')
 });
 
 module.exports = User;
